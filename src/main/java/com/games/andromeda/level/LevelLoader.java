8dec359656584eb6be23643509a02ec1c78c629e
McFeod <macfeod@yandex.ru>
2016-09-27 22:09:19 +0500

    new map format with vertex indexes

--- src/main/java/com/games/andromeda/level/LevelLoader.java
+++ src/main/java/com/games/andromeda/level/LevelLoader.java
@@ -1,37 +1,32 @@
 package com.games.andromeda.level;
 
 import android.content.Context;
-import android.graphics.PointF;
+import android.util.SparseArray;
 
 import com.games.andromeda.graph.Edge;
 import com.games.andromeda.graph.MyGraph;
 import com.games.andromeda.graph.Node;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class LevelLoader {
     public class MapFormatException extends Exception {}
-    private Map<PointF, Node> map;
+    private SparseArray<Node> map;
     private MyGraph result;
 
     private LevelLoader(){
-        map = new HashMap<>();
+        map = new SparseArray<>();
         result = new MyGraph();
     }
 
-    private void addVertex(float x, float y, Node.SystemType type){
-        PointF point = new PointF(x, y);
-        Node node = new Node(point.x, point.y, type);
-        map.put(point, node);
+    private void addVertex(Integer id, Float x, Float y, Node.SystemType type){
+        Node node = new Node(x, y, type);
+        map.put(id, node);
         result.addNode(node);
     }
 
-    private void addEdge(float fromX, float fromY, float toX, float toY, int weight){
-        PointF first = new PointF(fromX, fromY);
-        PointF second = new PointF(toX, toY);
-        result.addEdge(new Edge(map.get(first), map.get(second), weight));
+    private void addEdge(Integer firstId, Integer secondId, Integer weight){
+        result.addEdge(new Edge(map.get(firstId), map.get(secondId), weight));
     }
 
 
@@ -39,14 +34,18 @@ public class LevelLoader {
         if (line == null){
             return false;
         }
-        switch(line.length){
-            case 2:
-                addVertex(Float.parseFloat(line[0]), Float.parseFloat(line[1]), Node.SystemType.EMPTY);
+        switch(line[0]){
+            case "V":
+                addVertex(
+                        Integer.parseInt(line[1]),
+                        Float.parseFloat(line[2]),
+                        Float.parseFloat(line[3]),
+                        Node.SystemType.EMPTY);
                 break;
-            case 5:
-                addEdge(Float.parseFloat(line[0]), Float.parseFloat(line[1]),
-                        Float.parseFloat(line[2]), Float.parseFloat(line[3]),
-                        Integer.parseInt(line[4]));
+            case "E":
+                addEdge(Integer.parseInt(line[1]),
+                        Integer.parseInt(line[2]),
+                        Integer.parseInt(line[3]));
                 break;
             default:
                 throw new MapFormatException();
